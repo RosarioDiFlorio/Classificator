2794a6e50f522d25ac7c8dce7491c25babef8f88
Rosario <r.diflorio93@gmail.com>
2017-08-04 13:11:30 +0200

    first commit

---
+++ DataSourceClassificator/src/main/java/eu/innovation/engineering/DataSourceClassificator/App.java
@@ -0,0 +1,13 @@
+package eu.innovation.engineering.DataSourceClassificator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
