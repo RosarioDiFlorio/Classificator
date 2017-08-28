5401c217bef38555bbe8402ecb29c0c3aa4abed5
Rosario <Rosario@DESKTOP-CHMMSKB>
2017-08-28 13:00:39 +0200

    CSVBuilder finished

--- DataSourceClassificator/src/main/java/eu/innovation/engineering/prepocessing/CSVBuilder.java
+++ DataSourceClassificator/src/main/java/eu/innovation/engineering/prepocessing/CSVBuilder.java
@@ -3,6 +3,7 @@ package eu.innovation.engineering.prepocessing;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -13,22 +14,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.innovation.engineering.prepocessing.dictionaries.ClusteringKMeans;
 import eu.innovation.engineering.prepocessing.dictionaries.Dictionary;
 import eu.innovation.engineering.prepocessing.featurextractor.FeatureExtractor;
+import eu.innovation.engineering.prepocessing.util.Configurator;
 import eu.innovation.engineering.prepocessing.util.Features;
 import eu.innovation.engineering.prepocessing.util.IdAndTarget;
 import eu.innovation.engineering.prepocessing.util.Paper;
 import eu.innovation.engineering.prepocessing.util.configurator.PathConfigurator;
 
-public class DictionaryBuilder {
+public class CSVBuilder {
 
   private ObjectMapper mapper;
 
 
-  public DictionaryBuilder() {
+  public CSVBuilder() {
     mapper = new ObjectMapper();
   }
 
 
-  public void buildDictionaries() throws IOException{
+  public void buildCSV() throws IOException{
     ClusteringKMeans clusteringDictionaries = new ClusteringKMeans();
     HashMap<String, Dictionary> dictionaries = clusteringDictionaries.clusterWithDatasourceAsItems();
 
@@ -79,6 +81,9 @@ public class DictionaryBuilder {
 
     mapper.writerWithDefaultPrettyPrinter().writeValue(new File(PathConfigurator.dictionariesFolder+"dictionaryForTraining.json"), featuresPapersTrainingWithTarget );
     mapper.writerWithDefaultPrettyPrinter().writeValue(new File(PathConfigurator.dictionariesFolder+"dictionaryForTraining.json"), featuresPapersTestWithTarget );
+
+    createDatasetPython(featuresPapersTrainingWithTarget,categories,"train");
+    createDatasetPython(featuresPapersTestWithTarget,categories,"test");
   }
 
   //METODO CHE RESITUISCE UN HASHMAP DI CATEGORIA, PER OGNI CATEGORIA LA LISTA DI PAPER CHE APPARTENGONO
@@ -125,14 +130,65 @@ public class DictionaryBuilder {
     return toReturn;
   }
 
+
+  //METODO CHE SCRIVE I FILE CSV
+  private static void createDatasetPython(HashMap<IdAndTarget, ArrayList<Features>> featuresPapersTrainingWithTarget, HashSet<String> categories, String fileName) throws IOException {
+    //Per ogni categoria creo una folder
+    String firstLine="labels,id";
+
+    File csvFile = new File(PathConfigurator.pyCSVFolder+fileName+".csv");
+    csvFile.createNewFile();
+    PrintWriter pWriterCSV = new PrintWriter(csvFile);
+    
+    for(int i=0;i<Configurator.numFeatures;i++)
+      firstLine+=",F"+i;
+    
+    pWriterCSV.println(firstLine);
+
+
+    for(String category : categories){
+      File dir = new File(PathConfigurator.pyFolder+category);
+      dir.mkdir();
+      for(IdAndTarget idAndTarget : featuresPapersTrainingWithTarget.keySet()){
+        if(idAndTarget.getTarget().equals(category)){
+          FileWriter writer = new FileWriter(PathConfigurator.pyFolder+category+"/"+idAndTarget.getId()+".txt");
+          String keywordsToWrite=category+","+idAndTarget.getId()+",";
+          Iterator iterator = featuresPapersTrainingWithTarget.get(idAndTarget).iterator();
+
+          Features feature = (Features) iterator.next();
+
+          keywordsToWrite+=feature.getScore();
+
+          do{
+            //keywordsToWrite+="";
+            Features feature2 = (Features) iterator.next();
+            keywordsToWrite+=","+feature2.getScore();
+          }
+          while(iterator.hasNext());
+
+
+          pWriterCSV.println(keywordsToWrite);
+          writer.write(keywordsToWrite);
+          writer.flush();
+          writer.close();
+        }
+      }
+
+    }
+
+    pWriterCSV.flush();
+    pWriterCSV.close();
+  }
+
+
   /**
    * Example Main
    * @param args
    * @throws IOException
    */
   public static void main(String[] args) throws IOException{
-    DictionaryBuilder db = new DictionaryBuilder();
-    db.buildDictionaries();
+    CSVBuilder db = new CSVBuilder();
+    db.buildCSV();
   }
 
 
--- DataSourceClassificator/src/main/java/eu/innovation/engineering/prepocessing/util/configurator/PathConfigurator.java
+++ DataSourceClassificator/src/main/java/eu/innovation/engineering/prepocessing/util/configurator/PathConfigurator.java
@@ -4,11 +4,13 @@ package eu.innovation.engineering.prepocessing.util.configurator;
 public class PathConfigurator {
 
   public static final String datasetFolder = "data/datasets/";
-  public static final String trainingAndTestFolder = "data/datasets/TrainingAndTest";
+  public static final String trainingAndTestFolder = "data/datasets/TrainingAndTest/";
   public static final String applicationFileFolder = "data/application/";
   public static final String backUpDatasetFolder = "data/datasets/backup/";
   public static final String dictionariesFolder = "data/datasets/dictionaries/";
   public static final String keywordExtractorsFolder = "../KeywordExtractor/";
+  public static final String pyCSVFolder ="data/datasets/python/csv/";
+  public static final String pyFolder = "data/datasets/python/";
   
   
 }
