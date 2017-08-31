af675de86f63e4a8ae3eb7a0964b1d406004f063
Luigi Lomasto <l.lomasto@innovationengineering.eu>
2017-08-31 15:49:21 +0200

    added new file txt

--- DataSourceClassificator/src/main/java/eu/innovation/engineering/start/Start.java
+++ DataSourceClassificator/src/main/java/eu/innovation/engineering/start/Start.java
@@ -11,8 +11,17 @@ public class Start {
 
   private static final boolean readDictionaryFileTXT = true;
 
-
-  public static void main(String[] args) throws IOException {
+  public static void main(String args){
+    
+    
+  }
+  
+  public static void mainForCSV(){
+    
+  }
+  
+  
+  public static void mainOnlyForDictionaries() throws IOException {
 
     // CREAZIONE DEI DIZIONARI
 
