0cfb73355b377e6693976b526b3a1cc27a64ca9a
Rosario <Rosario@DESKTOP-CHMMSKB>
2017-09-05 10:13:59 +0200

    added lowThreshold and upperThreshold in the results analysis

--- Tester/src/main/java/eu/innovationengineering/Tester/CSVReader.java
+++ Tester/src/main/java/eu/innovationengineering/Tester/CSVReader.java
@@ -5,13 +5,18 @@ package eu.innovationengineering.Tester;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
 
 public class CSVReader {
 
-  public static void main(String[] args) {
+  
+  public static void main(String[] args){
+    read("results2.csv");
+  }
+  
+  
+  public static void read(String csvFile) {
 
-    String csvFile = "listPt.csv";
+   
     String line = "";
     String cvsSplitBy = ",";
 
@@ -20,13 +25,9 @@ public class CSVReader {
       while ((line = br.readLine()) != null) {
 
         // use comma as separator
-        String[] country = line.split(cvsSplitBy);
-
-
-        Lemmatizer lemmsTest = new Lemmatizer();
-        List<String> lemmas =  lemmsTest.lemmatize(country[1]);
+        String[] country = line.split(cvsSplitBy); 
         
-        System.out.println(country[1]+" -> " + lemmas);
+        System.out.println(country[0]+" -> ");
 
       }
 
