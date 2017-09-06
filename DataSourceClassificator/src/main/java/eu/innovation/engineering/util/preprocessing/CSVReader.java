1cbf51e2dfc7e4ca42ea93d65cb73dad8777040f
Rosario <Rosario@DESKTOP-CHMMSKB>
2017-09-06 11:35:48 +0200

    create a new function that create a dataset of training from the csv result file of the python classifier.

--- DataSourceClassificator/src/main/java/eu/innovation/engineering/util/preprocessing/CSVReader.java
+++ DataSourceClassificator/src/main/java/eu/innovation/engineering/util/preprocessing/CSVReader.java
@@ -4,6 +4,7 @@ package eu.innovation.engineering.util.preprocessing;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -23,18 +24,29 @@ import eu.innovation.engineering.keyword.extractor.interfaces.KeywordExtractor;
 public class CSVReader {
   private static final int numKey = 10;
 
+
   public static void main(String[] args) throws Exception{
-    float uThreshold = (float) 0.8;
+    float uThreshold = (float) 1.0;
     float lThreshold = (float) 0.7;
-    String category = Configurator.Categories.religion_and_spirituality.name();
-//    category = "none";
-    int count = readResultClassifier(PathConfigurator.applicationFileFolder+"results.csv",lThreshold,uThreshold,category,true);
-    System.out.println("numero di source "+count);
+    createTrainingSetFromCsvResults(PathConfigurator.applicationFileFolder+"results.csv",lThreshold,uThreshold);
+  }
+
+
+  public static void mainSingleTest(String[] args) throws Exception{
+    float uThreshold = (float) 1.0;
+    float lThreshold = (float) 0.7;
+    String category = Configurator.Categories.education.name();
+    category = "none";
+    //3115
+    int count = readResultClassifier(PathConfigurator.applicationFileFolder+"results.csv",lThreshold,uThreshold,category,true,0);
+    System.out.println("category -> "+category);
+    System.out.println("Founded sources -> "+count);
+    System.out.println("Upper threshold -> "+uThreshold +"\nLower threshold -> "+lThreshold);
   }
 
 
 
-  public static int readResultClassifier(String csvFile, float lowThreshold,float upperThreshold,String category,boolean withKeys) throws Exception{
+  public static int readResultClassifier(String csvFile, float lowThreshold,float upperThreshold,String category,boolean isCount,int batchLine) throws Exception{
     KeywordExtractor kex = new InnenExtractor(PathConfigurator.keywordExtractorsFolder);
     Map<String, List<String>> dataMap = read(csvFile);
 
@@ -42,7 +54,7 @@ public class CSVReader {
     ids.addAll(dataMap.keySet());
 
     SolrClient solr = new SolrClient();
-    PrintWriter p = new PrintWriter(new File(PathConfigurator.applicationFileFolder+"filetocheck"+category+".txt"));
+    PrintWriter p = new PrintWriter(new File(PathConfigurator.applicationTestFolder+"filetocheck"+category+".txt"));
     int count = 0;
     String idToInsert= "";
     for(String id: dataMap.keySet()){
@@ -50,39 +62,37 @@ public class CSVReader {
       try{
         probs = Float.parseFloat(dataMap.get(id).get(0));
       }catch (Exception e) {
+        e.printStackTrace();
         continue;
       }
 
-      if(probs <= upperThreshold && probs >= lowThreshold ){
-        //count++;
-        //System.out.println(probs);
+      if(probs <= upperThreshold && probs >= lowThreshold ){      
         List<String> tmp  = new ArrayList<>();
         tmp.add(id);
-
         if(dataMap.get(id).get(1).contains(category) || category.equals("none")){
+
+          if(count < batchLine || isCount){
+            System.out.println(count);
+            count++;
+            continue;
+          }
+
           List<Source> sources = solr.getSourcesFromSolr(tmp, Paper.class);
-          
 
           for(Source s: sources){
-            if(!dataMap.get(s.getId()).get(1).contains(category) && !category.equals("none"))
-              continue;
-            else{
-              count++;
-              System.out.println(count);
+            count++;
+            System.out.println(count);
+            idToInsert += s.getId()+" 1\n";
+            p.println(s.getId()+" - "+dataMap.get(s.getId()).get(0)+" - "+dataMap.get(s.getId()).get(1));
+            p.println(kex.extractKeywordsFromText(s.getTexts(), numKey).stream().map(Keyword::getText).collect(Collectors.toList())+"\n");
+            p.println(s.getTitle());
+            p.println(s.getTexts().get(1));
+            p.println("-------------------------------------\n");
 
-              
-              idToInsert += s.getId()+" 1\n";
-              p.println(s.getId()+" - "+dataMap.get(s.getId()).get(0)+" - "+dataMap.get(s.getId()).get(1));
-              if(withKeys)
-                p.println(kex.extractKeywordsFromText(s.getTexts(), numKey).stream().map(Keyword::getText).collect(Collectors.toList())+"\n");
-              p.println(s.getTitle());
-              p.println(s.getTexts().get(1));
-              p.println("-------------------------------------\n");
-            }         
           }
-          p.flush();
-        }   
-      }
+        }
+        p.flush();
+      }   
     }
     p.println("\n"+idToInsert);
     p.flush();
@@ -91,23 +101,68 @@ public class CSVReader {
   }
 
 
+  public static void createTrainingSetFromCsvResults(String csvFile, float lowThreshold,float upperThreshold) throws FileNotFoundException{
+
+    Map<String, List<String>> dataMap = read(csvFile);
+    
+    HashMap<String, List<String>> categoryMap = new HashMap<>();
+    
+    float probs = 0;
+    for(String id : dataMap.keySet()){
+      try{
+        probs = Float.parseFloat(dataMap.get(id).get(0));
+      }catch (Exception e) {
+        e.printStackTrace();
+        continue;
+      }
+      
+      if(probs <= upperThreshold && probs >= lowThreshold ){
+        String category = dataMap.get(id).get(1);
+        if(categoryMap.containsKey(category)){
+          List<String> tmp = categoryMap.get(category);
+          tmp.add(id);
+          categoryMap.put(category, tmp);
+        }else{
+          List<String> tmp = new ArrayList<>();
+          tmp.add(id);
+          categoryMap.put(category, tmp);
+        }       
+      }       
+    }
+
+
+    PrintWriter p = new PrintWriter(new File(PathConfigurator.trainingAndTestFolder+"trainingDatasetFromCsvResult.txt"));
+    for(String category: categoryMap.keySet()){
+      p.println("/"+category.replace("_", " "));
+      for(String id: categoryMap.get(category)){
+        p.println(id+" 1");
+      }
+      p.println("\n");
+      p.flush();
+    }
+    p.close();
+  }
+
 
   public static Map<String,List<String>> read(String csvFile) {
-
-
     String line = "";
     String cvsSplitBy = ",";
     Map<String, List<String>> dataMap = new HashMap<>();
     try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+      
       while ((line = br.readLine()) != null) {
 
         // use comma as separator
         String[] csvData = line.split(cvsSplitBy); 
         List<String> data = new ArrayList<>();
         for(int i =1;i<csvData.length;i++){
-          data.add(csvData[i]);
+            data.add(csvData[i].trim());
+            
         }
-        dataMap.put(csvData[0], data);
+        String key = csvData[0].trim();
+        dataMap.put(key, data);
+        
+          
       }
 
       return dataMap;
