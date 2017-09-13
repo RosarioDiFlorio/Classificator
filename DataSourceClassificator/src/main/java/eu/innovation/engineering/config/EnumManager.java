9be352261550cf36006ad521fb3ced1a3e24551c
Rosario <Rosario@DESKTOP-CHMMSKB>
2017-09-13 17:00:25 +0200

    enumManager for the next enums

---
+++ DataSourceClassificator/src/main/java/eu/innovation/engineering/config/EnumManager.java
@@ -0,0 +1,30 @@
+package eu.innovation.engineering.config;
+
+public class EnumManager {
+  
+  
+
+  
+  
+  public enum FirstCategories{
+    travel,
+    technology_and_computing,
+    finance,
+    art_and_entertainment,
+    science,
+    business_and_industrial,
+    food_and_drink,
+    society,
+    education,
+    religion_and_spirituality,
+    medicine
+  }
+  
+  public enum ScienceCategories{
+    chemistry_biology,
+    geology,
+    mathematics,
+    physics,
+    astronomy 
+  }
+}
