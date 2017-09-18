7ee74ada5d427d503deccb3a1dbfd558f384fc09
Luigi Lomasto <l.lomasto@innovationengineering.eu>
2017-09-18 12:48:27 +0200

    commit to merge

--- KeywordExtractor/src/main/java/eu/innovation/engineering/LSA/keywordExtractor/LSAKeywordExtractor.java
+++ KeywordExtractor/src/main/java/eu/innovation/engineering/LSA/keywordExtractor/LSAKeywordExtractor.java
@@ -1,14 +1,19 @@
 package eu.innovation.engineering.LSA.keywordExtractor;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.SingularValueDecomposition;
 
 import com.ibm.watson.developer_cloud.alchemy.v1.model.Keyword;
 
 import eu.innovation.engineering.keyword.extractor.interfaces.KeywordExtractor;
 import eu.innovationengineering.nlp.analyzer.stanfordnlp.StanfordnlpAnalyzer;
+import eu.innovationengineering.nlp.beans.AnnotatedWord;
+import eu.innovationengineering.nlp.beans.oat.SentenceChunk;
 
 /**
  * @author Rosario
@@ -47,43 +52,150 @@ public class LSAKeywordExtractor implements KeywordExtractor {
    * @param chunks
    * @return
    */
-  public static MatrixRepresentation buildMatrixA(List<String> chunks){
-    
+  public static MatrixRepresentation buildMatrixA(List<SentenceChunk> chunks){
+
     List<String> wordList = new ArrayList<String>();
-    
-    for(String chunk : chunks){
-      if(!wordList.contains(chunk)){
-          wordList.add(chunk);
+    Array2DRowRealMatrix matrix = new Array2DRowRealMatrix();
+
+    //crea la lista di word
+    for(SentenceChunk chunk : chunks){
+      for(AnnotatedWord word : chunk.getWords()){
+        if(!wordList.contains(word.getWord())){
+          wordList.add(word.getWord());
+        }
       }
     }
-    
-    
-    
-    
-    return null;
+
+    //crea la matrice
+    int row=0;
+    int column=0;
+
+    for(String word : wordList){
+      column=0;
+      for(SentenceChunk chunk : chunks){
+        matrix.addToEntry(row, column, Tf(word, chunks, column)*Isf(word,chunks));
+        column++;
+      }
+      row++;
+    }
+
+    MatrixRepresentation matrixA = new MatrixRepresentation();
+    matrixA.setMatrixA(matrix);
+    matrixA.setTokenList(wordList);
+
+    return matrixA;
   }
 
   /**
+   * return matrix U after SVD decomposition
    * @param <E>
    * @return toDefine
    */
   public static Array2DRowRealMatrix SVD(MatrixRepresentation matrixA){  
-    return null;
+    
+    SingularValueDecomposition svd = new SingularValueDecomposition(matrixA.getMatrixA());
+    
+    
+    return (Array2DRowRealMatrix) svd.getU();
   }
-  
-  
-  private static  List<Keyword> getKeywordList(MatrixRepresentation matrixA, Array2DRowRealMatrix SVDResult){
-    return null;
+
+
+  /**
+   * Return keywordList from matrix U after SVD decomposition
+   * @param matrixA
+   * @param SVDResult
+   * @return
+   */
+  private static  List<Keyword> getKeywordList(MatrixRepresentation matrixA, Array2DRowRealMatrix U, int threshold){
+    
+    double[] bestColumn = U.getColumn(0);
+    
+    HashMap<Integer,Double> bestIndex = new HashMap<Integer,Double>();
+    List<Keyword> keywordList = new ArrayList<Keyword>();
+    
+    if(threshold<=bestColumn.length){
+      while(threshold>0){
+       int index= max(bestColumn);
+       bestIndex.put(index,bestColumn[index]);
+       bestColumn[index]=0;
+       threshold--;
+      }
+    }
+    else
+    {
+      System.out.println("Threshold value is greater then column size");
+      return null;
+    }
+    
+    for(int index : bestIndex.keySet()){
+      Keyword k = new Keyword();
+      k.setText(matrixA.getTokenList().get(index));
+      k.setRelevance(bestIndex.get(index));
+      keywordList.add(k);
+    }
+    
+    return keywordList;
   }
-  
-  
-  private static float Tf(String word,List<String>chunks){
-    return 0;
+
+
+  /**
+   * return position of max value into double array 
+   * @param bestColumn
+   * @return
+   */
+  private static int max(double[] bestColumn) {
+    double max = 0;
+    int indexMax=0;
+    for(int i=0; i<bestColumn.length;i++){
+      if(bestColumn[i]>max){
+        max = bestColumn[i];
+        indexMax = i;
+      }
+    }
+    
+    return indexMax;
+    
   }
-  
-  private static float Isf(String word,List<String> chunks){
-    return 0;
+
+  /**
+   * return TF value
+   * @param word
+   * @param chunks
+   * @param j
+   * @return
+   */
+  private static float Tf(String word,List<SentenceChunk> chunks, int j){
+    
+    SentenceChunk sentenceJ = chunks.get(j);
+    //number of times word i in sentence j
+    int countSentenceJ=0;
+    for(AnnotatedWord tmpWord : sentenceJ.getWords())
+      if(tmpWord.getWord().equals(word))
+        countSentenceJ++;
+     
+    return countSentenceJ/sentenceJ.getWords().size();
   }
-  
-  
+
+  /**
+   * return ISF value
+   * @param word
+   * @param chunks
+   * @return
+   */
+  private static float Isf(String word,List<SentenceChunk> chunks){
+    
+    //number of sentences with word i
+    int numberSentenceWithWord = 0;
+    
+    for(SentenceChunk sentence : chunks){
+      List<String> wordList = sentence.getWords().stream().map(AnnotatedWord::getWord).collect(Collectors.toList());
+      if(wordList.contains(word))
+        numberSentenceWithWord++;
+    }
+    
+    
+    return chunks.size()/numberSentenceWithWord;
+  }
+
+
 }
