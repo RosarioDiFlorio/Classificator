250b25c20fbe3a6e6dac4ce09b393d9574a75923
Rosario <r.diflorio93@gmail.com>
2017-11-15 10:20:14 +0100

    committing

---
+++ wikipedia/src/main/java/eu/innovation/engineering/wikipedia/PathConfigurator.java
@@ -0,0 +1,22 @@
+package eu.innovation.engineering.wikipedia;
+
+
+public class PathConfigurator {
+
+  public static final String datasetFolder = "data/datasets/";
+  public static final String wikipediaFolder = "data/wikipedia/";
+  public static final String rootFolder = "data/root/";
+  public static final String trainingAndTestFolder = "data/datasets/TrainingAndTest/";
+  public static final String applicationFileFolder = "data/application/";
+  public static final String backUpDatasetFolder = "data/datasets/backup/";
+  public static final String dictionariesFolder = "data/datasets/dictionaries/";
+  public static final String keywordExtractorsFolder = "../KeywordExtractor/";
+  public static final String pyCSVFolder ="data/datasets/python/csv/";
+  public static final String pyFolder = "datasetPy/";
+  public static final String applicationTestFolder= applicationFileFolder+"test/";
+  public static final String applicationResultFolder = applicationFileFolder+"results/";
+  public static final String categories = "data/application/dictionariesCategory/";
+  
+  
+  
+}
