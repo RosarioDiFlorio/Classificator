3d6027557a54309f29e9832eb2566ba3d5b1a254
Luigi <Luigi@DESKTOP-OAJHCFE.homenet.telecomitalia.it>
2017-12-06 10:27:13 +0100

    added method to change narrower-broader dependency

--- renovationTaxonomy/src/main/java/datasetCreatorFromTaxonomy/renovationTaxonomy/App.java
+++ renovationTaxonomy/src/main/java/datasetCreatorFromTaxonomy/renovationTaxonomy/App.java
@@ -1,5 +1,7 @@
 package datasetCreatorFromTaxonomy.renovationTaxonomy;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,13 +10,19 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.BrokenBarrierException;
 
+import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.rdf.model.impl.PropertyImpl;
+import com.hp.hpl.jena.rdf.model.impl.ResourceImpl;
+import com.hp.hpl.jena.rdf.model.impl.StatementImpl;
 import com.hp.hpl.jena.util.FileManager;
 
 
@@ -26,35 +34,43 @@ public class App
 	public static void main(String [] args) throws IOException{
 		final String  inputFileName = "wheesbee_taxonomy_general_1.0.skos.rdf";
 		Model model = ModelFactory.createDefaultModel();
-
 		InputStream in = FileManager.get().open(inputFileName);
 		if (in == null) {
 			throw new IllegalArgumentException ( "File: " + inputFileName + " not found");
 		}
-
 		model.read(new InputStreamReader(in), "");
 		in.close();
-
 		StmtIterator statementList = model.listStatements();
-
-		Statement statement = statementList.next();
-		Set<Resource> resourceList = new HashSet<Resource>();
-		resourceList.add(model.getResource("http://www.wheesbee.eu/taxonomy#100145"));
-
-		model = delete(model,resourceList);
 		
+		
+		
+		
+		//PER CAMBIARE I BROADER ED I NARROWER
+		model = changeProperty(model,statementList);
+
+
+		//PER ELIMINARE I CONCEPT CHE NON SERVONO
+		BufferedReader br2 = new BufferedReader(new FileReader("categoriesToRemove.txt"));
+		String readed2 = br2.readLine();
+		Set<Resource> resourceList = new HashSet<Resource>();
+		while(readed2!=null){
+			System.out.println(readed2);
+			resourceList.add(model.getResource(readed2));
+			model = delete(model,resourceList);
+			readed2 = br2.readLine();
+		}
 		FileWriter writer = new FileWriter("newWheesbee.rdf");
 		model.write(writer);
-		
+
 	}
 
 
-/**
- * method to delete concept tree from root 
- * @param model
- * @param resourceList
- * @return
- */
+	/**
+	 * method to delete concept tree from root 
+	 * @param model
+	 * @param resourceList
+	 * @return
+	 */
 	public static Model delete(Model model, Set<Resource> resourceList){
 
 		Set<Resource> narrowersNode = new HashSet<Resource>();
@@ -72,12 +88,12 @@ public class App
 			}
 
 		}
-		
+
 		//IF NARROWER SET IS NOT EMPY, MAKE RECURSIVE TASK
 		if(narrowersNode.size()>0)
 			model = delete(model,narrowersNode);
-		
-		
+
+
 		// SECOND PHASE, TO DELETE CURRENT STATEMENT ABOUT CURRENT RESOURCE
 		List<Statement> statementToRemove = new ArrayList<Statement>();
 		StmtIterator statementList2 = model.listStatements();
@@ -87,11 +103,74 @@ public class App
 				if(statement.getSubject().getNameSpace().equals(resource.getNameSpace()) || statement.getObject().toString().equals(resource.getNameSpace())){
 					statementToRemove.add(statement);
 					System.out.println(statement.toString());
-					}
+				}
 			}
 		}
-		
+
 		model.remove(statementToRemove);
+
+		return model;
+	}
+	
+	
+	public static Model changeProperty(Model model, StmtIterator statementList) throws IOException{
+		Statement statement = null;
+		BufferedReader br = new BufferedReader(new FileReader("categoriesToModify.txt"));
+		String readed = br.readLine();
+		List<Statement> statementToRemove = new ArrayList<Statement>();
+		List<Statement> statementToAdd = new ArrayList<Statement>();
+		List<String> idToChange = new ArrayList<String>();
+		
+		while(readed!=null){
+			boolean added = false;
+			String []components= readed.split(",");
+			while(statementList.hasNext()){
+				statement = statementList.next();
+				if(statement.getSubject().getNameSpace().equals(components[0]) && statement.getPredicate().getLocalName().equals("broader")){
+					added=true;					
+					//Creo il nuovo statement
+					ResourceImpl object = new ResourceImpl(components[1]);
+					Statement newStatement = new StatementImpl(statement.getSubject(), statement.getPredicate(), object);
+					statementToRemove.add(statement);
+					statementToAdd.add(newStatement);
+					
+					//creo lo statement inverso, per il link da padre a figlio
+					PropertyImpl property = new PropertyImpl("http://www.w3.org/2004/02/skos/core#narrower");
+					ResourceImpl subject = new ResourceImpl(components[1]);
+					Statement newStatement2 = new StatementImpl(subject,  property, statement.getSubject());
+					statementToAdd.add(newStatement2);
+					
+				}
+			}
+			if(!added){ // allora significa che era un topConcept che diventa figlio
+				ResourceImpl subject = new ResourceImpl(components[0]);
+				PropertyImpl propertyNarrower = new PropertyImpl("http://www.w3.org/2004/02/skos/core#narrower");
+				ResourceImpl object = new ResourceImpl(components[1]);
+				PropertyImpl propertyBroader = new PropertyImpl("http://www.w3.org/2004/02/skos/core#broader");
+				Statement statementBroaderToNarrower = new StatementImpl(subject,propertyBroader,object);
+				Statement statementNarrowerToBroader = new StatementImpl(object,propertyNarrower,subject);
+				
+				statementToAdd.add(statementNarrowerToBroader);
+				statementToAdd.add(statementBroaderToNarrower);
+				
+			}
+			model.remove(statementToRemove);
+			statementToRemove = new ArrayList<Statement>();
+			statementList = model.listStatements();
+			readed = br.readLine();
+		}
+		
+		//RIMUOVO TUTTI I VECCHI NARROWER DEI VECCHI GENITORI , PER ROMPERE LA DIPENDENZA DEI CONCETTI CON I VECCHI BROADER
+		while(statementList.hasNext()){
+			
+			
+		}
+		
+		
+		
+		
+		
+		model.add(statementToAdd);
 		
 		return model;
 	}
