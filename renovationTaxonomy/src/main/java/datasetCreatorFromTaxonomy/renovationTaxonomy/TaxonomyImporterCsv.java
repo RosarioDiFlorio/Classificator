0e471280c47e8b5df585679e92b5c589a32d33dd
Rosario Di Florio <r.diflorio93@gmail.com>
2017-12-12 11:34:07 +0100

    merging

--- renovationTaxonomy/src/main/java/datasetCreatorFromTaxonomy/renovationTaxonomy/TaxonomyImporterCsv.java
+++ renovationTaxonomy/src/main/java/datasetCreatorFromTaxonomy/renovationTaxonomy/TaxonomyImporterCsv.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,17 +19,7 @@ public class TaxonomyImporterCsv {
       while ((line = br.readLine()) != null) {
         // use comma as separator
         String[] csvData = line.split(cvsSplitBy); 
-        List<String> data = new ArrayList<>();
-        if(csvData.length>=2){
-          for(int i =0;i<csvData.length-1;i++){
-            data.add(csvData[i].trim());
-          }
-          String key = csvData[csvData.length-1].trim().replace("en.wikipedia.org/wiki/", "");
-          if(dataMap.containsKey(key))
-            System.out.println(key);
-          if(!key.equals(""))
-            dataMap.put(key, data);
-        }
+
       }
     }
   }
