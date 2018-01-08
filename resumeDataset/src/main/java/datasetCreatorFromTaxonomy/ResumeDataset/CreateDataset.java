ceb59bfadda5f1a41dbcf6f08d5637b8dcc9c081
Luigi <Luigi@DESKTOP-OAJHCFE.homenet.telecomitalia.it>
2018-01-08 17:51:22 +0100

    added write to save labels for test dataset into CreateDataset class

--- resumeDataset/src/main/java/datasetCreatorFromTaxonomy/ResumeDataset/CreateDataset.java
+++ resumeDataset/src/main/java/datasetCreatorFromTaxonomy/ResumeDataset/CreateDataset.java
@@ -1,12 +1,23 @@
 package datasetCreatorFromTaxonomy.ResumeDataset;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class CreateDataset {
 
@@ -14,20 +25,20 @@ public class CreateDataset {
 	public static void main(String[] args) throws IOException{
 		ListAllFiles fileReader = new ListAllFiles();
 
-		String directoryWindows ="D:/Development/Datasets/dataset_500xleaf_2012017/";
+		String directoryWindows ="D:/TextClassifier/dataset_tassonomia/";
 
 
 		ArrayList<String> fileList = (ArrayList<String>) fileReader.listFilesAndFilesSubDirectories(directoryWindows, new ArrayList<String>());
 		Set<String> pathList = new HashSet<String>();
 
-		String basePathSrc = "D:/Development/Datasets/dataset_500xleaf_2012017/";
-		String basePathDst = "D:/Development/Datasets/dataset_training/";
+		String basePathSrc = "D:/TextClassifier/dataset_tassonomia/";
+		String basePathDst = "D:/Development/Datasets/dataset_trainingNew/";
 
-		new File("D:/TextClassifier/datasets_training").mkdir();
-		new File("D:/TextClassifier/datasets_test").mkdir();
-		String basePathDstTraining = "D:/TextClassifier/datasets_training/";
-		String basePathDstTest = "D:/TextClassifier/datasets_test/";
-		
+		new File("D:/TextClassifier/datasets_trainingNew").mkdir();
+		new File("D:/TextClassifier/datasets_testNew").mkdir();
+		String basePathDstTraining = "D:/TextClassifier/datasets_trainingNew/";
+		String basePathDstTest = "D:/TextClassifier/datasets_testNew/";
+
 		/*new File("D:/TextClassifier/training/training").mkdir();
 		new File("D:/TextClassifier/training/test").mkdir();
 		String basePathDstTraining = "D:/TextClassifier/training/trainingApp/";
@@ -52,19 +63,35 @@ public class CreateDataset {
 			}
 			else
 				if(length==0){
-					//System.out.println(splitted[0]);
 					pathList.add("root/"+splitted[0]);
 				}
 
 		}
 
-		// PER OGNNI PATH CALCOLATO 
+		
+
+		//TRAINING
+		ArrayList<String> added = buildDataset(pathList, basePathDstTraining, basePathSrc, directoryWindows, fileList, fileReader,450, new ArrayList<String>(), "training");
+
+		//TEST
+		buildDataset(pathList, basePathDstTest, basePathSrc, directoryWindows, fileList, fileReader,50,added,"test");
+	}
+
+
+
+
+
+
+	public static ArrayList<String>  buildDataset(Set<String> pathList,String basePathDst,String basePathSrc, String directoryWindows, ArrayList<String> fileList, ListAllFiles fileReader, int numSourceToCopy, ArrayList<String> added, String datasetType) throws IOException{
+		// PER OGNNI PATH CALCOLATO
+		
+		ArrayList<String> addedToReturn = new ArrayList<String>();
+		int countDuplicate = 0;
 		for(String path:pathList){
 			System.out.println(path);
 
 			//creo la folder 
-			new File(basePathDstTraining+path).mkdir();
-			new File(basePathDstTest+path).mkdir();
+			new File(basePathDst+path).mkdir();
 			Set<String> leafList = new HashSet<String>();
 
 			// CALCOLO LA LISTA DI TUTTE LE CATEGORIE FOGLIA
@@ -78,124 +105,67 @@ public class CreateDataset {
 			}
 
 			// A QUESTO PUNTO AGGIUNGO I DOCUMENTI AL PATH CORRENTE USANDO LE CATEGORIE FOGLIA CALCOLATE
-
-			int numSource = (500/leafList.size())*3;
-
+			int numSource = (numSourceToCopy/leafList.size());
+			
+			FileWriter writerLabelsTest = new FileWriter(new File("labelsItemTest.csv"));
+			AnalyzerWikipediaGraph analyzerWikipedia = new AnalyzerWikipediaGraph();
 			for(String leaf:leafList){
-				
+
 				String [] leafSplitted = leaf.split("/");
 				String nameLeaf = leafSplitted[leafSplitted.length-1];
 
-				int count = 0, countTraining = 0,countTest = 0;
+				int count = 0; 
 				ArrayList<String> files = (ArrayList<String>) fileReader.listFilesAndFilesSubDirectories(directoryWindows+"/"+leaf, new ArrayList<String>());
 				for(String file:files){
-					if(count>= numSource)
-						break;
-					else{
-						count++;
-						file = file.replace("\\", "/");
-						String[] splitted = file.split("/");
-						File f1 = new File(file);
-						File f2 = null;
-						if(countTest<(numSource*0.1)){
-							f2 = new File(basePathDstTest+path+"/"+nameLeaf+"_"+splitted[splitted.length-1]);
-							countTest++;
-						}
+					file = file.replace("\\", "/");
+					String[] splitted = file.split("/");
+
+					if(!added.contains(splitted[splitted.length-1])){
+						addedToReturn.add(splitted[splitted.length-1]);
+						if(count>=numSource)
+							break;
 						else{
-							f2 = new File(basePathDstTraining+path+"/"+nameLeaf+"_"+splitted[splitted.length-1]);
-							countTraining++;
+							count++;
+							File f1 = new File(file);
+							File f2 = null;
+							if(datasetType.equals("training")){
+								f2 = new File(basePathDst+path+"/"+nameLeaf+"_"+splitted[splitted.length-1]);
+							}
+							else{
+								f2 = new File(basePathDst+"/"+splitted[splitted.length-1]);
+								// Codice per cercare le categori dal grafo wikipedia. Creare un file CSV che contiene le categorie che il grafo ha restituito
+								List<String>labels =  analyzerWikipedia.getDocumentLabels(splitted[splitted.length-1],3);
+								writerLabelsTest.write(splitted[splitted.length-1]+","+labels.get(0)+","+labels.get(1)+","+labels.get(2)+"\n");
+								writerLabelsTest.flush();
+							}
+							FileUtils.copyFile(f1, f2);  
 						}
-						FileUtils.copyFile(f1, f2);
-
 					}
 				}
-				
 			}
+			writerLabelsTest.flush();
+			writerLabelsTest.close();
 		}
-
+		
+		return addedToReturn;
 	}
 
-	public static void main2(String [] args) throws IOException{
-		ListAllFiles fileReader = new ListAllFiles();
-		String directoryWindows ="D:/TextClassifier/dataset_tassonomia";
-		String newDirectory = "D:/TextClassifier/datasets";
-
-		ArrayList<String> fileList = (ArrayList<String>) fileReader.listFilesAndFilesSubDirectories(directoryWindows, new ArrayList<String>());
-		int numDocuments = 1000;
-		for(String file : fileList){
-			String basePathSrc = "D:/TextClassifier/dataset_tassonomia/";
-			String basePathDst = "D:/TextClassifier/training/datasets/";
-			file = file.replace("\\", "/").replace(basePathSrc, "");
-			String [] splitted = file.split("/");
-			String name = splitted[splitted.length-1];
-			if(splitted.length>2){
-				int length = splitted.length-2;
-
-				File f1 = new File(basePathSrc+file);
-				new File(basePathDst+splitted[length-1]).mkdir();
-				new File(basePathDst+splitted[length-1]+"/"+splitted[length]).mkdir();
-				System.out.println(basePathDst+splitted[length-1]+"/"+splitted[length]);
-				if(fileReader.listFolders(basePathDst+splitted[length-1]+"/"+splitted[length]).size()<numDocuments){
-					File f3 = new File(basePathDst+splitted[length-1]+"/"+splitted[length]+"/"+name);
-					FileUtils.copyFile(f1,f3);
-					length--;
-					while(length>0){
-						new File(basePathDst+splitted[length-1]).mkdir();
-						new File(basePathDst+splitted[length-1]+"/"+splitted[length]).mkdir();
-
-						File f2 = new File(basePathDst+splitted[length-1]+"/"+splitted[length]+"/"+name);
-						FileUtils.copyFile(f1,f2);
-						length--;
-					}
-					//sono arrivato alla fine quindi aggungo a root
-					new File(basePathDst+"root").mkdir();
-					new File(basePathDst+"root/"+splitted[0]).mkdir();
-					File f2 = new File(basePathDst+"root/"+splitted[0]+"/"+name);
-					FileUtils.copyFile(f1,f2);
-
-				}
-			}
-			//Caso in cui la categoria è ti tipo law/nomefile.txt ovvero c'è solo la top category
-			else{
-				System.out.println(file);
-				File f1 = new File(basePathSrc+file);
-				new File(basePathDst+"root").mkdir();
-				new File(basePathDst+"root/"+splitted[0]).mkdir();
-				if(fileReader.listFolders(basePathDst+"root/"+splitted[0]).size()<numDocuments){	
-					File f2 = new File(basePathDst+"root/"+splitted[0]+"/"+name);
-					FileUtils.copyFile(f1,f2);
-				}
-			}
-
-
-			/*for(int i = 0; i<splitted.length-2;i++){
-					File f1 = new File(basePathSrc+file);
-					if(i==0){
-						//Root
-						new File(basePathDst+"root").mkdir();
-						new File(basePathDst+"root/"+splitted[i]).mkdir();
-						File f2 = new File(basePathDst+"root/"+splitted[i]+"/"+name);
-						FileUtils.copyFile(f1,f2);
-						//Primo Livello
-						new File(basePathDst+splitted[i]).mkdir();
-						new File(basePathDst+splitted[i]+"/"+splitted[i+1]).mkdir();
-						File f3 = new File(basePathDst+splitted[i]+"/"+splitted[i+1]+"/"+name);
-						FileUtils.copyFile(f1,f3);
-					}
-					else{
-						//Altri livelli
-						new File(basePathDst+splitted[i]).mkdir();
-						new File(basePathDst+splitted[i]+"/"+splitted[i+1]).mkdir();
-						File f3 = new File(basePathDst+splitted[i]+"/"+splitted[i+1]+"/"+name);
-						FileUtils.copyFile(f1,f3);
-
-					}
-				}*/
 
 
 
-		}
-	}
+
+
+
+
+
+
+
+
+
+
+	
+
+
 }
 
 
