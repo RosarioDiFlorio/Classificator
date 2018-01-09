0a3505455084f838e39d48edc569aefcd8e409ac
Rosario Di Florio <r.diflorio93@gmail.com>
2018-01-09 15:51:10 +0100

    get documents labels taxonomy

--- resumeDataset/src/main/java/datasetCreatorFromTaxonomy/ResumeDataset/AnalyzerWikipediaGraph.java
+++ resumeDataset/src/main/java/datasetCreatorFromTaxonomy/ResumeDataset/AnalyzerWikipediaGraph.java
@@ -1,5 +1,7 @@
 package datasetCreatorFromTaxonomy.ResumeDataset;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -18,33 +20,64 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 public class AnalyzerWikipediaGraph {
-  
+
   private static  HashMap<String, AdjacencyListRow> adjacencyList = null;
+  private static HashMap<String,Set<String>> mappingTaxonomyWikipedia = null;
+
+
 
   public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException{
-    String testDocument = "45712";
-    System.out.println(getDocumentLabels(testDocument));
+    String testDocument = "47393352";
+    System.out.println(getDocumentLabelsTaxonomy(testDocument));
   }
- 
+
+
+  public static List<String> getDocumentLabelsTaxonomy(String idDocument) throws IOException{
+    if(mappingTaxonomyWikipedia == null){
+      mappingTaxonomyWikipedia = new HashMap<>();
+      Map<String, List<String>> taxonomyCsv = getTaxonomyCSV("wheesbee_taxonomy.csv");
+      for(String wikiCat: taxonomyCsv.keySet()){
+        Set<String> toAdd = new HashSet<>();
+        toAdd.add(taxonomyCsv.get(wikiCat).get(taxonomyCsv.get(wikiCat).size()-1));
+        if(!mappingTaxonomyWikipedia.containsKey(wikiCat))
+          mappingTaxonomyWikipedia.put(wikiCat, toAdd);
+        else
+          mappingTaxonomyWikipedia.get(wikiCat).addAll(toAdd);
+      }
+    }
+
+    List<String> toReturn = new ArrayList<>();
+    List<String> wikipediaLabels = getDocumentLabels(idDocument);
+    for(String label: wikipediaLabels){
+      if(mappingTaxonomyWikipedia.containsKey(label))
+        toReturn.addAll(mappingTaxonomyWikipedia.get(label));
+    }
+
+    return toReturn;
+
+  }
+
+
   public static List<String> getDocumentLabels(String idDocument) throws IOException{
 
     Set<String> documentCategories = getParentCategoriesByIdPage(idDocument);
     Set<PathInfo> results = new HashSet<PathInfo>();
-    
+
     if(adjacencyList == null)    
       adjacencyList = CrawlerWikipediaCategory.returnAdjacencyListFromFile("signedGraphWikipedia");
 
-    
-    
+
+
     for(String category: documentCategories){
       results.addAll(searchNearestMarkedVertex(adjacencyList, category, 3));
     }
-    
+
     List<PathInfo> orderedResults = new ArrayList<PathInfo>(results);
     Collections.sort(orderedResults,Collections.reverseOrder());
+    //    System.out.println(orderedResults);
     return orderedResults.stream().map(e->e.getName()).collect(Collectors.toList());
   }
-  
+
 
   /**
    * Return the categories of a Wikipedia's page.
@@ -54,36 +87,36 @@ public class AnalyzerWikipediaGraph {
    * @throws IOException
    */
   public static Set<String> getParentCategoriesByIdPage(String idDocument) throws IOException{
-      Set<String> toReturn  = new HashSet<>();
-      JsonArray categoriesParent = null;
-      String parentsURL = "https://en.wikipedia.org/w/api.php?action=query&pageids="+idDocument+"&prop=categories&clshow=!hidden&cllimit=500&indexpageids&format=json";
-      JsonObject responseParent = CrawlerWikipediaCategory.getJsonResponse(parentsURL);
-      //build ids array 
-      JsonArray idsJsonArray = responseParent.get("query").getAsJsonObject().get("pageids").getAsJsonArray();
-      ArrayList<String> ids = new ArrayList<String>();
-      for (JsonElement e : idsJsonArray){
-          if (Integer.parseInt(e.getAsString())>0){
-              ids.add(e.getAsString());
-          }
+    Set<String> toReturn  = new HashSet<>();
+    JsonArray categoriesParent = null;
+    String parentsURL = "https://en.wikipedia.org/w/api.php?action=query&pageids="+idDocument+"&prop=categories&clshow=!hidden&cllimit=500&indexpageids&format=json";
+    JsonObject responseParent = CrawlerWikipediaCategory.getJsonResponse(parentsURL);
+    //build ids array 
+    JsonArray idsJsonArray = responseParent.get("query").getAsJsonObject().get("pageids").getAsJsonArray();
+    ArrayList<String> ids = new ArrayList<String>();
+    for (JsonElement e : idsJsonArray){
+      if (Integer.parseInt(e.getAsString())>0){
+        ids.add(e.getAsString());
       }
-      for(String id : ids){
-          try{
-              categoriesParent = responseParent.getAsJsonObject().get("query").getAsJsonObject().get("pages").getAsJsonObject().get(id).getAsJsonObject().get("categories").getAsJsonArray();
-              String title = responseParent.getAsJsonObject().get("query").getAsJsonObject().get("pages").getAsJsonObject().get(id).getAsJsonObject().get("title").getAsString();
-              if(categoriesParent!=null){
-                  // add all vertex obtained to hashset
-                  for(JsonElement cat : categoriesParent){
-                      String name = cat.getAsJsonObject().get("title").getAsString();
-                      String [] namesplitted = name.replaceAll(" ", "_").split("Category:");
-                      toReturn.add(namesplitted[1]);
-                  }
-              }
+    }
+    for(String id : ids){
+      try{
+        categoriesParent = responseParent.getAsJsonObject().get("query").getAsJsonObject().get("pages").getAsJsonObject().get(id).getAsJsonObject().get("categories").getAsJsonArray();
+        String title = responseParent.getAsJsonObject().get("query").getAsJsonObject().get("pages").getAsJsonObject().get(id).getAsJsonObject().get("title").getAsString();
+        if(categoriesParent!=null){
+          // add all vertex obtained to hashset
+          for(JsonElement cat : categoriesParent){
+            String name = cat.getAsJsonObject().get("title").getAsString();
+            String [] namesplitted = name.replaceAll(" ", "_").split("Category:");
+            toReturn.add(namesplitted[1]);
           }
-          catch(Exception e){
-              System.out.println(id+": hasn't parents category --- URL: "+parentsURL);
-          }
+        }
       }
-      return toReturn;
+      catch(Exception e){
+        System.out.println(id+": hasn't parents category --- URL: "+parentsURL);
+      }
+    }
+    return toReturn;
   }
 
   /**
@@ -146,24 +179,58 @@ public class AnalyzerWikipediaGraph {
           }
         }else // porzione aggiunta per possibile errore di formato delle chiavi all'interno della lista di adiacenze. 
           if(adjacencyList.containsKey(vertex.getName().replace("_", " "))){
-          vertex.setName(vertex.getName().replace("_", " "));
-          if(adjacencyList.get(vertex.getName()).isTaxonomyCategory()){
-            nearestMarkedVertex.add(vertex);
-            countMarked++;
-            if(countMarked >= numberOfMarkedVertex)
-              return nearestMarkedVertex;
+            vertex.setName(vertex.getName().replace("_", " "));
+            if(adjacencyList.get(vertex.getName()).isTaxonomyCategory()){
+              nearestMarkedVertex.add(vertex);
+              countMarked++;
+              if(countMarked >= numberOfMarkedVertex)
+                return nearestMarkedVertex;
+            }
+            //aggiungo i prossimi nodi da visitare
+            for(String v : adjacencyList.get(vertex.getName()).getLinkedVertex()){
+              PathInfo vInfo = new PathInfo(v, vertex.getValue()+1);
+              if(!visitedVertex.contains(vInfo) && !vertexToVisit.contains(vInfo))
+                vertexToVisit.add(vInfo);
+            }
           }
-          //aggiungo i prossimi nodi da visitare
-          for(String v : adjacencyList.get(vertex.getName()).getLinkedVertex()){
-            PathInfo vInfo = new PathInfo(v, vertex.getValue()+1);
-            if(!visitedVertex.contains(vInfo) && !vertexToVisit.contains(vInfo))
-              vertexToVisit.add(vInfo);
-          }
-        }
       }
     }
     //ritorno la lista di nodi marcati.
     return nearestMarkedVertex;
   }
+
+
+  /**
+   * read Category by Taxonomy CSV. Input file contains all categories used from Taxonomy
+   * @param csvFile
+   * @param labeled
+   * @return 
+   * @return
+   * @throws IOException
+   */
+  public static  Map<String,List<String>> getTaxonomyCSV(String csvFile) throws IOException{
+
+    String line = "";
+    String cvsSplitBy = ",";
+    Map<String, List<String>> dataMap = new HashMap<String, List<String>>();
+
+    BufferedReader br = new BufferedReader(new FileReader(csvFile));
+
+    while ((line = br.readLine()) != null) {
+      // use comma as separator
+      String[] csvData = line.split(cvsSplitBy); 
+      List<String> data = new ArrayList<String>();
+      if(csvData.length>=2){
+        for(int i =0;i<csvData.length-1;i++){
+          data.add(csvData[i].trim());
+        }
+        String key = csvData[csvData.length-1].trim().replace("en.wikipedia.org/wiki/Category:", "");
+        if(!key.equals(""))
+          dataMap.put(key, data);
+      }
+    }
+    return dataMap;
+  } 
+
 
 }
