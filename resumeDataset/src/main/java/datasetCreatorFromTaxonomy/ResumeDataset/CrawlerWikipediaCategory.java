35632360aa2eee97a8790bada310b35bf507567c
Luigi <Luigi@DESKTOP-OAJHCFE.homenet.telecomitalia.it>
2018-01-15 14:42:16 +0100

    commit cosineSimilarityInverse

--- resumeDataset/src/main/java/datasetCreatorFromTaxonomy/ResumeDataset/CrawlerWikipediaCategory.java
+++ resumeDataset/src/main/java/datasetCreatorFromTaxonomy/ResumeDataset/CrawlerWikipediaCategory.java
@@ -416,6 +416,26 @@ public class CrawlerWikipediaCategory {
 		
 		return crawlerResult.getAdjacencyList();
 	}
+	
+	
+	
+	public static double cosineSimilarityInverse(float[] vectorA, float[] vectorB) {
+	    double dotProduct = 0.0;
+	    double normA = 0.0;
+	    double normB = 0.0;
+	    if(vectorA!=null && vectorB!=null && vectorA.length==vectorB.length){
+	      for (int i = 0; i < vectorA.length; i++) {
+	        dotProduct += vectorA[i] * vectorB[i];
+	        normA += vectorA[i] * vectorA[i];
+	        normB += vectorB[i] * vectorB[i];
+	      }   
+	    }
+
+	    if(dotProduct == 0 || (normA * normB) == 0)
+	      return 0;
+	    else
+	      return Math.asin((dotProduct) / (Math.sqrt(normA * normB)));
+	  }
 
 
 	/**
