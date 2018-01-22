229ee47360c889f2275decc8b9e96fe9be483674
Luigi <Luigi@DESKTOP-OAJHCFE>
2018-01-22 11:44:10 +0100

    commit for merge

---
+++ EvaluationClassifier/src/test/java/datasetCreatorFromTaxonomy/EvaluationToClassifier/EvaluationCSVTest.java
@@ -0,0 +1,38 @@
+package datasetCreatorFromTaxonomy.EvaluationToClassifier;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class EvaluationCSVTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public EvaluationCSVTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( EvaluationCSVTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
